85be191a94f369be7ae746b21566aaca181b36c9
Kathy Luo <kathyluo18@gmail.com>
2025-02-24 13:43:23 +0100

    chore: add placeholder test

---
+++ test/Divvi.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+
+const CONTRACT_NAME = 'Divvi'
+
+describe(CONTRACT_NAME, () => {
+  it('works with a placeholder test', () => {
+    expect(true).to.be.true
+  })
+})
