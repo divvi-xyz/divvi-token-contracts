1f525bef7c4517ed005033661e7d6ca412a833fd
Kathy Luo <kathyluo18@gmail.com>
2025-02-25 11:58:27 +0100

    fix: comments

--- contracts/Divvi.sol
+++ contracts/Divvi.sol
@@ -1,21 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC20Upgradeable} from '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+import {ERC20PermitUpgradeable} from '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol';
 import {AccessControlDefaultAdminRulesUpgradeable} from '@openzeppelin/contracts-upgradeable/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol';
 import {UUPSUpgradeable} from '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
-import {Initializable} from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 
 contract DivviToken is
-  Initializable,
-  ERC20Upgradeable,
+  ERC20PermitUpgradeable,
   AccessControlDefaultAdminRulesUpgradeable,
   UUPSUpgradeable
 {
   bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
-  bool public upgradesDisabled;
-
-  error UpgradesDisabled();
 
   function initialize(address owner, uint48 transferDelay) public initializer {
     __ERC20_init('Divvi', 'DIVVI');
@@ -23,18 +18,11 @@ contract DivviToken is
     __UUPSUpgradeable_init();
   }
 
-  /// @notice Mints tokens, callable only by MINTER_ROLE
   function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
     _mint(to, amount);
   }
 
   function _authorizeUpgrade(
     address
-  ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {
-    if (upgradesDisabled) revert UpgradesDisabled();
-  }
-
-  function disableUpgrades() external onlyRole(DEFAULT_ADMIN_ROLE) {
-    upgradesDisabled = true;
-  }
+  ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {} // solhint-disable-line no-empty-blocks
 }
